1724b14342bfd668109bee117d707997ea8174d7
kaesinol <keiplyer@gmail.com>
2025-01-07 21:00:23 +0800

    :bookmark:v1.0.2

--- yamlgenerator-hook-search/tests/__init__.py
+++ yamlgenerator-hook-search/tests/__init__.py
@@ -2,8 +2,10 @@ from unittest import TestCase
 
 from gameyamlspiderandgenerator.util.fgi import template_dict
 from yamlgenerator_hook_search import Search
+from gameyamlspiderandgenerator.util.config import config
 
-
+config.hook_configs ={'search': {'apple': ...,
+                                 'google-play': ...}}
 class Test(TestCase):
     def test_search(self):
         print(Search().setup({**template_dict, 'name': 'hollow knight', }))
